f126707de51363308b8d0e925f69d2ad84100181
vinay jain <vinayjain61787@gmail.com>
2024-04-12 10:46:49 +0530

    Created the Blog Page

--- infinity/src/features/blogs/pages/BlogPage.jsx
+++ infinity/src/features/blogs/pages/BlogPage.jsx
@@ -1,13 +1,252 @@
 import React from 'react'
 import NavBar from '../../../components/navBars/NavBar'
+import Footer from '../../../components/navBars/Footer'
+
 const BlogPage = () => {
   return (
     <div>
       <div>
         <NavBar />
-        <div>
-          
+
+        <div className='page'>
+          <div className='inline-flex flex-row'>
+
+            <div className='col w-3/12'>
+              <div className='topics-col m-6 p-6 bg-blue-300 rounded-3xl'>
+                <div className='text-xl'>Similar Topics</div>
+                <ul className='my-4'>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                  <li className='my-4'><div className='inline-flex flex-row'><p className='topics-name mr-3'>Web Technologies</p><p className='number-articles text-sm'>37.3k+ articles</p></div></li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='col w-6/12'>
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-3 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-light-blue'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+              <div className='cards bg-blue-300 rounded-3xl flex flex-row p-6 my-6 content-between'>
+
+                  <div className='card w-7/12'>
+                    <p className='my-4 text-xl'>System Design Interview and Answers</p>
+                    <div className=''>
+                      <p className='my-4'>In the software engineering interview process system design round has become a standard part of the interview. If you want to get your dream</p>
+                    </div>
+                    <div className='flex flex-row'>
+                      <img src='/' className='mx-2' />
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-blue-400'>GBlog</a>
+                      <a href='/' className='mx-2 p-1 rounded-2xl bg-blue-400'>System Design</a>
+                    </div>
+                  </div>
+
+                  <div className='w-5/12'>
+                    <i className=''>Last Updated: 09 February 2024</i>
+                    <img src='/' className=' bg-light-blue'/>
+                  </div>
+
+              </div>
+
+
+
+            </div>
+
+            <div className='col w-3/12'>
+              <div className='bg-blue-300 m-6 rounded-3xl'>
+              <img src='/' />
+              </div>
+            </div>
+
+          </div>
         </div>
+
+        <Footer />
       </div>
     </div>
   )
